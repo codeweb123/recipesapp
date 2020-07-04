b7466ff0158a4221a14f2c600d39cbfb58e1c5f6
codeweb123 <jiyonj@hotmail.com>
2020-07-03 23:53:01 -0400

    add validations to models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
+    before_save { self.email = email.downcase }
+    validates :username, presence: true, length: { maximum: 35 }
+    validates :email, presence: true, length: { maximum: 200 }, uniqueness: { case_sensitive: false }
+
+
 
 end
